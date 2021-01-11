ecb74e0812b8fba458a15a71e3d50a31044102d4
AnwarAhmed <stormyshadow2009@outlook.com>
2021-01-11 17:10:34 +0200

    update the style of writing notes

---
+++ ES6/Modules/main.js
@@ -0,0 +1,5 @@
+//import our module sample and call it on the console.
+
+import hello from './modules/test.js';
+
+console.log(hello());
---
+++ ES6/Modules/modules/test.js
@@ -0,0 +1,7 @@
+//export a simple function to log hello world in the console
+
+function hello(){
+    return `hello world!`;
+}
+
+export default hello;
