bac56e511ac173b274ac0c63619c97866f0a4dd3
AnwarAhmed <stormyshadow2009@outlook.com>
2021-03-23 15:41:31 +0200

    serving html file

---
+++ Express/StartServer.js
@@ -0,0 +1,39 @@
+//we use the old way of import require
+var express = require('express');
+//declare our app
+var app = express();
+//In Express, routes takes the following structure: app.METHOD(PATH, HANDLER).
+//METHOD is an http method in lowercase. 
+//PATH is a relative path on the server (it can be a string, or even a regular expression).
+//HANDLER is a function that Express calls when the route is matched. 
+//Handlers take the form function(req, res) {...}, 
+//where req is the request object, and res is the response object. For example, the handler
+/*function(req, res) {
+    res.send('Response String');
+  }*/
+  app.get("/",(req,res)=>{
+      res.send("Hello Express")
+  })
+
+  //serving html file
+  //notice the absolute path with sendFile method
+  //absolutePath = __dirname + relativePath/file.ext
+  app.get("/",function(req,res){
+      res.sendFile(__dirname + "src/index.html")
+  })
+
+  /*the static assets needed by your application (stylesheets, scripts, images).
+  we can use middleware for it:
+  >>> express.static(path), 
+  where the path parameter is the absolute path of the folder containing the assets.*/
+
+  /*
+  middleware needs to be mounted using the method app.use(path, middlewareFunction).
+  The first path argument is optional. If you don’t pass it, 
+  the middleware will be executed for all requests.
+  */
+  //normal usage:
+  app.use(express.static(__dirname + "/public"));
+
+  //Assets at the /public route
+  app.use("/public", express.static(__dirname + "/public"));
