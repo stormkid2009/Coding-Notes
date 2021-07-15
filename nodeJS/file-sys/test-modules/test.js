b78780a4bed504059c74b0b50ec1359991740fc4
AnwarAhmed <stormyshadow2009@outlook.com>
2021-07-15 14:46:20 +0200

    import and export data via mongodb

--- nodeJS/file-sys/test-modules/test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const updater = require('./updateFile');
-const deleteHelper = require('./delete');
-//updater('./test.json',{name:"farah"});
-deleteHelper('./test.json',{name:"ali"})
