e1f1192e84e640f6ba618befe859dc871a3b0fff
AnwarAhmed <stormyshadow2009@outlook.com>
2021-12-25 10:20:35 +0200

    update getStaticPaths

--- NextJs/pre-rendering/static-generation/fetchData.js
+++ NextJs/pre-rendering/static-generation/fetchData.js
@@ -13,7 +13,20 @@ export default function UsersList({users}) {
         </div>
     )
 }
+//async function which tell next-js how many pages it should pre-render
+//important paths key is required
+export async function getStaticPaths(){
+    return {
+        paths:[
+            {params:{pageId:`path of page`}},
+            {params:{pageId:`path of page`}},
+            {params:{pageId:`path of page`}},
+        ],
+        fallback:false,
+    }
+}
 //async function which run in build time to send its props to our page
+//important props key is required
 
 export async function getStaticProps(){
     const response = await fetch('API URL HERE');
